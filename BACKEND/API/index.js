71e30f9cc980202a6cceba4584f2325b5507589c
ulisses <uli.felix8484@gmail.com>
2025-05-05 19:43:25 -0300

    Update index.js

--- BACKEND/API/index.js
+++ BACKEND/API/index.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
  next();
 });
 app.use(express.json());
-const PORT = "https://apitarefas2.com";
+const PORT = 'http://localhost:3000';
 const routes = require('./routes/routes');
 app.use('/api', routes);
 app.listen(PORT, () => {
